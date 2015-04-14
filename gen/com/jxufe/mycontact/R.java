cda1f6fcfcaa4c04dcb04336e9ff7fad3ecc8fae
Administrator <Administrator@w101_16>
2015-04-14 11:54:18 +0800

    login

--- gen/com/jxufe/mycontact/R.java
+++ gen/com/jxufe/mycontact/R.java
@@ -21,13 +21,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background_login=0x7f020000;
+        public static final int background_login_div=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int linearlayout1=0x7f080000;
+        public static final int linearlayout2=0x7f080002;
+        public static final int user_name=0x7f080001;
+        public static final int user_password=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int avtivity_login=0x7f030001;
+        public static final int background_login=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -64,5 +72,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int normalText=0x7f060002;
     }
 }
