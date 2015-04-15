4aff56f22cb0799b005c7df3bf5aab5063d84486
Administrator <Administrator@w201_35>
2015-04-15 11:44:12 +0800

    aaa

--- gen/com/jxufe/mycontact/R.java
+++ gen/com/jxufe/mycontact/R.java
@@ -26,11 +26,15 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int linearlayout1=0x7f080000;
-        public static final int linearlayout2=0x7f080002;
-        public static final int user_name=0x7f080001;
-        public static final int user_password=0x7f080003;
+        public static final int TV=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int button=0x7f080001;
+        public static final int linearlayout1=0x7f080002;
+        public static final int linearlayout2=0x7f080004;
+        public static final int register_link=0x7f080007;
+        public static final int signin_button=0x7f080006;
+        public static final int user_name=0x7f080003;
+        public static final int user_password=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -44,6 +48,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int login_label_password=0x7f050006;
+        public static final int login_label_signin=0x7f050004;
+        public static final int login_label_username=0x7f050005;
+        public static final int login_register_link=0x7f050007;
+        public static final int welcome=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/jxufe/mycontact/contacts/activity/MainActivity.java
+++ src/com/jxufe/mycontact/contacts/activity/MainActivity.java
@@ -1,25 +1,64 @@
 package com.jxufe.mycontact.contacts.activity;
 
-import com.jxufe.mycontact.R;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+
+import com.jxufe.mycontact.R;
+import com.jxufe.mycontact.login.activity.Login_Avtivity;
 
 public class MainActivity extends Activity {
-
+	boolean IsLogIn = false;
+	Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);  
         setContentView(R.layout.activity_main);
+        
+        bt = (Button)findViewById(R.id.button);
+        bt.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(IsLogIn == true){
+		        	run();
+		        }
+		        else{
+		        	login();
+		        }
+			}
+		});
+        
     }
 
 
     @Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		//login();
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    public void login(){
+    	Intent login_intent =new Intent(MainActivity.this,Login_Avtivity.class);
+    	this.startActivity(login_intent);
+    }
+    public void run(){
+    	
     }
     
 }
